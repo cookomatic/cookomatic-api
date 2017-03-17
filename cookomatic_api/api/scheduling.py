78ff349179e633697c8d2ef797bbb4faec28e042
amham17 <amham@ncsu.edu>
2017-03-17 15:15:29 -0400

    Added the mocked schedule for the EOH meal (#24)
    * Added the mocked schedule for the EOH meal
    
    * Fixed the build error
    
    * Remove PyCharm files

--- cookomatic_api/api/scheduling.py
+++ cookomatic_api/api/scheduling.py
@@ -14,29 +14,73 @@ def generate_mock_schedule(meal_id):
     return {
         'meal_id': meal_id,
         'steps': [
-            Step(name='Step 1',
-                 description='This is Step 1.',
-                 estimated_time=random.randrange(1, 10),
-                 dish_id=random.randrange(0, 999999),
-                 ingredients=[
-                     'Cheese',
-                     'Bacon'
-                 ]).to_dict(),
-            Step(name='Step 2',
-                 description='This is Step 2.',
-                 estimated_time=random.randrange(1, 10),
-                 dish_id=random.randrange(0, 999999),
-                 ingredients=[
-                     'Alfredo Sauce',
-                     'Pasta'
-                 ]).to_dict(),
-            Step(name='Step 3',
-                 description='This is Step 3.',
-                 estimated_time=random.randrange(1, 10),
+            Step(name='Marinate Chicken',
+                 description='Place chicken and seasonings into a gallon sized bag.',
+                 estimated_time=1,
                  dish_id=random.randrange(0, 999999),
                  ingredients=[
                      'Chicken',
-                     'Marinade'
+                     'Seasonings'
+                 ]).to_dict(),
+            Step(name='Preheat Oven',
+                 description='Preheat Oven to 400 degrees',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999)).to_dict(),
+            Step(name='Heat Nonstick Skillet',
+                 description='Place a large nonstick skillet over medium high heat and wait until warm.',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999)).to_dict(),
+            Step(name='Season Carrots',
+                 description='Add the carrots to a bowl and toss with remaining seasonings',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Carrots',
+                     'Seasonings'
+                 ]).to_dict(),
+            Step(name='Add Chicken to Skillet',
+                 description='Add chicken to skillet and cook over medium heat for 30 seconds.',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Chicken'
+                 ]).to_dict(),
+             Step(name='Flip Chicken',
+                 description='Flip the chicken and cook for an additional 30 seconds',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Chicken'
+                 ]).to_dict(),
+             Step(name='Remove Chicken From Skillet',
+                 description='Remove browned chicken from the pan .',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Chicken'
+                 ]).to_dict(),
+            Step(name='Add Pineapple and Rice',
+                 description='Add pineapple and rice to skillet, cover and reduce to medium low, and cook for 45 seconds.',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Pineapple',
+                     'Rice'
+                 ]).to_dict(),
+            Step(name='Bake Carrots',
+                 description='Place the carrots on a baking tray and put in oven. Cook for 1 minute.',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Carrots'
+                 ]).to_dict(),
+            Step(name='Add Chicken to Skillet',
+                 description='Stir rice and nestle chicken on top and cook for an additional minute.',
+                 estimated_time=1,
+                 dish_id=random.randrange(0, 999999),
+                 ingredients=[
+                     'Chicken',
+                     'Rice'
                  ]).to_dict(),
         ]
     }
